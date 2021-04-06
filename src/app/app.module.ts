5ee74422017908bf5a509e617eefb7464973ec43
Frederique Renet <frederique.renet@free.fr>
2021-04-06 18:07:38 +0200

    Structure: 404 page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home.component';
+import { BasicComponent } from './routes/basic.component';
+import { AdvancedComponent } from './routes/advanced.component';
+import { ContainedComponent } from './routes/contained.component';
+import { PageNotFoundComponent } from './pageNotFound.component';
+
+const animationRoutes:Routes = [
+  {
+    path: 'animation-home',
+    component: HomeComponent
+  },
+  {
+    path: 'animation-basics',
+    component: BasicComponent
+  },
+  {
+    path: 'animation-contained',
+    component: ContainedComponent
+  },
+  {
+    path: 'animation-advanced',
+    component: AdvancedComponent
+  },
+  {
+    path: '',
+    redirectTo: '/animation-home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BasicComponent,
+    AdvancedComponent,
+    ContainedComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(animationRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pageNotFound.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <h1>404 - Page Not Found</h1>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">Sorry - this page does not exist.</div>
+      </div>
+    </div>
+  `,
+  styles: [``]
+})
+
+export class PageNotFoundComponent {}
