0f8791d714ea0e624ed1b111b8e889e2d619ec29
Frederique Renet <frederique.renet@free.fr>
2021-04-09 16:13:53 +0200

    stepped animation with keyframes

--- src/app/animbox.component.ts
+++ src/app/animbox.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'animbox',
@@ -15,26 +15,38 @@ import { trigger, state, style, animate, transition } from '@angular/animations'
   `],
   animations: [
     trigger('changeState', [
-        state('original', style({
-            backgroundColor: '#47748f',
-            transform: 'scale(1)'
-        })),
-        state('basic', style({
-            backgroundColor: '#440000',
-            transform: 'scale(2)'
-        })),
-        state('delaying', style({
-            backgroundColor: '#812170',
-            transform: 'scale(1.6)'
-        })),
-        state('easing', style({
-            backgroundColor: '#985b00',
-            transform: 'scale(2.2)'
-        })),
-        transition('* => basic', animate('800ms')),
-        transition('* => original', animate('200ms')),
-        transition('* => delaying', animate('800ms 1200ms ease-out')),
-        transition('* => easing', animate('800ms ease-in-out')),
+      state('original', style({
+        backgroundColor: '#47748f',
+        transform: 'scale(1)'
+      })),
+      state('basic', style({
+        backgroundColor: '#440000',
+        transform: 'scale(2)'
+      })),
+      state('delaying', style({
+        backgroundColor: '#812170',
+        transform: 'scale(1.6)'
+      })),
+      state('easing', style({
+        backgroundColor: '#985b00',
+        transform: 'scale(2.2)'
+      })),
+      state('stepped', style({
+        backgroundColor: '#549a76',
+        transform: 'scale(1)'
+      })),
+      transition('* => basic', animate('800ms')),
+      transition('* => original', animate('200ms')),
+      transition('* => delaying', animate('800ms 1200ms ease-out')),
+      transition('* => easing', animate('800ms ease-in-out')),
+      transition('* => stepped', [
+        animate('3000ms ease-in-out', keyframes([
+          style({backgroundColor: '#dd9344', transform: 'scale(1.4)', offset: 0.2}),
+          style({backgroundColor: '#5c2346', transform: 'scale(0.8)', offset: 0.4}),
+          style({backgroundColor: '#1b1b1b', transform: 'scale(1.2)', offset: 0.7}),
+          style({backgroundColor: '#549a76', transform: 'scale(1)', offset: 0.9})
+        ]))
+      ])
     ])
   ]
 })
